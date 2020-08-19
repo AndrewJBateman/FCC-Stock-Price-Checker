52224bd390b5b5f4c5305588d04d50960425266a
Andrew Bateman <gomezbateman@yahoo.com>
2020-08-19 13:32:46 +0200

    stock price API replaced, readme updated

--- README.md
+++ README.md
@@ -1,11 +1,96 @@
-**FreeCodeCamp**- Information Security and Quality Assurance
-------
+# :zap: Nodejs-Stock-Prices
 
-Project Stock Price Checker
+* Node.js app to get stock price info from the [Financial Modeling Prep API](https://financialmodelingprep.com/) and store data in a MongoDB database as part of a FreeCodeCamp exercise for Front End Certification.
+
+*** Note: to open web links in a new window use: _ctrl+click on link_**
+
+## :page_facing_up: Table of contents
+
+* [:zap: Nodejs-Stock-Prices](#zap-nodejs-stock-prices)
+  * [:page_facing_up: Table of contents](#page_facing_up-table-of-contents)
+  * [:books: General info](#books-general-info)
+  * [:camera: Screenshots](#camera-screenshots)
+  * [:signal_strength: Technologies](#signal_strength-technologies)
+  * [:floppy_disk: Setup](#floppy_disk-setup)
+  * [:computer: Code Examples](#computer-code-examples)
+  * [:cool: Features](#cool-features)
+  * [:clipboard: Status & To-Do List](#clipboard-status--to-do-list)
+  * [:clap: Inspiration](#clap-inspiration)
+  * [:envelope: Contact](#envelope-contact)
+
+## :books: General info
+
+* Original instructions from FCC:
 
 1) SET NODE_ENV to `test` without quotes and set DB to your mongo connection string
 2) Complete the project in `routes/api.js` or by creating a handler/controller
 3) You will add any security features to `server.js`
 4) You will create all of the functional tests in `tests/2_functional-tests.js`
 
+* MongoDB Cloud Atlas database set up using google cloud
+* A different API was used from the one recommended to get stock price data : [Financial Modeling Prep API](https://financialmodelingprep.com/developer/docs/) was used.
 
+## :camera: Screenshots
+
+![Example screenshot](./images/stock.png).
+![Example screenshot](./images/mongodb.png).
+
+## :signal_strength: Technologies
+
+* [Node v12](https://nodejs.org/en/) javaScript runtime built on Chrome's V8 JavaScript engine
+* [mongoose v5](https://mongoosejs.com/) object modelling for node.js.
+* [Helmet v3](https://helmetjs.github.io/) Express.js security with HTTP headers.
+* [Cors v2](https://www.npmjs.com/package/cors) node.js package for providing Connect/Express middleware that can be used to enable CORS with various options.
+
+## :floppy_disk: Setup
+
+* Create MongoDB Atlas Cloud database (or local installed MongoDB database) and add user access/database credentials (USER_NAME, USER_PASSWORD, DB_CLUSTER & DB_NAME) to a new `.env` file. This is used in `server.js`.
+* Add IP address to MongoDB Atlas Network Access whitelist. Or simply whitelist all (IP address 0.0.0.0/0).
+* Run `node server.js` for a dev server. Navigate to `http://localhost:4000/`. The app will automatically reload if you change any of the source files.
+
+## :computer: Code Examples
+
+* extract from `server.js` to connect to MongoDB database
+
+```javascript
+// MongoDB Atlas Database Access Credentials
+const dbUserName = process.env.USER_NAME;
+const dbUserPass = process.env.USER_PASSWORD;
+const dbName = process.env.DB_NAME;
+const dbCluster = process.env.DB_CLUSTER
+const dbUrl = `mongodb+srv://${dbUserName}:${dbUserPass}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+
+// MongoDB Database connection
+mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  dbUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
+    if (err) {
+      console.log('Database connection error: ' + err);
+    } else {
+      console.log(
+        'Successful database connection to Mongoose Atlas database named: ',
+        dbName
+      );
+    }
+  }
+);
+```
+
+## :cool: Features
+
+* MongoDB cluster set up with username and password.
+
+## :clipboard: Status & To-Do List
+
+* Status: Working but can be improve
+* To-Do: improve
+
+## :clap: Inspiration
+
+* [Financial Modeling Prep API Documentation](https://financialmodelingprep.com/developer/docs/)
+
+## :envelope: Contact
+
+* Repo created by [ABateman](https://www.andrewbateman.org) - you are welcome to [send me a message](https://andrewbateman.org/contact)
--- server.js
+++ server.js
@@ -1,78 +1,90 @@
 'use strict';
-
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
-var helmet      = require('helmet');
-var mongoose    = require('mongoose');
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+var expect = require('chai').expect;
+var cors = require('cors');
+var helmet = require('helmet');
+var mongoose = require('mongoose');
+var apiRoutes = require('./routes/api.js');
+var fccTestingRoutes = require('./routes/fcctesting.js');
+var runner = require('./test-runner');
 var app = express();
 
-mongoose.set('useFindAndModify', false);
-//mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB, { useNewUrlParser: true }, (err, db) => {
-    if(err)
-    {
-      console.log('Database connection error: ' + err);
-    } 
-    else {
-      console.log('Successful database connection');
-    };
+// MongoDB Atlas Database Access Credentials
+const dbUserName = process.env.USER_NAME;
+const dbUserPass = process.env.USER_PASSWORD;
+const dbName = process.env.DB_NAME;
+const dbCluster = process.env.DB_CLUSTER
+const dbUrl = `mongodb+srv://${dbUserName}:${dbUserPass}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
 
-});
+// MongoDB Database connection
+mongoose.set('useFindAndModify', false);
+mongoose.connect(
+	dbUrl,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err, db) => {
+		if (err) {
+			console.log('Database connection error: ' + err);
+		} else {
+			console.log(
+				'Successful database connection to Mongoose Atlas database named: ',
+				dbName
+			);
+		}
+	}
+);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Challenge 1. Set the content security policies to only allow loading of scripts and css from your server.
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'"]
-    }
-}));
+// Challenge 1. Set the content security policies to only allow loading of scripts and css from your server.
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+			styleSrc: ["'self'"],
+		},
+	})
+);
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+// Display index.html page (static HTML) at '/'
+app.route('/').get(function (req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
+});
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
+// Routing for API
+apiRoutes(app);
+
+// 404 Not Found Middleware
 /*app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });*/
 
-//Start our server and tests!
+// Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
+	console.log('Listening on port ' + process.env.PORT);
+	if (process.env.NODE_ENV === 'test') {
+		console.log('Running Tests...');
+		setTimeout(function () {
+			try {
+				runner.run();
+			} catch (e) {
+				var error = e;
+				console.log('Tests are not valid:');
+				console.log(error);
+			}
+		}, 3500);
+	}
 });
 
-module.exports = app; //for testing
+module.exports = app; // for testing
